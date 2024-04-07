f564b296f7c72c3854e50f5b1fa9d64421aad02f
jessyovia <jessyovia100@gmail.com>
2024-04-07 15:48:11 -0700

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,35 @@
+
+#!/usr/bin/env bash
+# Install and Start Nginx with the updated config
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create HTML file
+FILE="<html>
+<head>
+</head>
+<body>
+    Holberton School
+</body>
+</html>"
+echo "$FILE" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+SERVER_CONFIG="
+server {
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+}"
+sudo bash -c "echo '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+
+sudo service nginx restart
+
